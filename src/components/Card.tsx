a6ebe91c4a89f0d55605fe338cfcbd0858f3d8b0
josejulian1604 <85717661+josejulian1604@users.noreply.github.com>
2025-06-30 21:29:08 -0600

    Mobile responsiveness updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,12 +5,12 @@ type CardProps = {
 
 export default function Card({ image, title }: CardProps) {
     return (
-        <div className="rounded-xl overflow-hidden bg-white shadow-md w-32 sm:w-36 md:w-40 lg:w-44 xl:w-40 2xl:w-50">
+        <div className="rounded-xl overflow-hidden bg-white shadow-md w-20 sm:w-36 md:w-40 lg:w-44 xl:w-40 2xl:w-50">
             <div className="p-2">
                 <img
                     src={image}
                     alt={title}
-                    className="w-full h-20 sm:h-32 md:h-36 lg:h-40 xl:h-30 2xl:h-40 object-cover"
+                    className="w-full h-10 sm:h-32 md:h-36 lg:h-40 xl:h-30 2xl:h-40 object-cover"
                 />
                 <footer className="mt-4 text-center font-medium text-black">
                     {title}
--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -13,13 +13,18 @@ export default function Gallery({ overlap }: GalleryProps) {
         <section
             className="bg-neutral-950 text-white relative z-10 p-10 shadow-lg transition-all duration-300 ease-in-out"
             // Aplica un margen negativo para superponer la galería con la rueda
-            style={{ marginTop: `-${overlap / 2}px` }}
+            style={{
+                marginTop:
+                    window.innerWidth < 640
+                        ? `-${overlap * 1.1}px`  
+                        : `-${overlap / 2}px`     
+            }}
         >
             <h1 className="text-3xl font-bold uppercase text-center">Galeria de Aventuras</h1>
             <div className="flex justify-center">
                 <CustomButton label="Ver galería" className="mt-4" />
             </div>
-            
+
             {/* Grid de imágenes en columnas responsivas */}
             <div className="mt-10 columns-1 sm:columns-2 md:columns-2 lg:columns-2 gap-4 space-y-4">
                 {images.map((_, index) => (
